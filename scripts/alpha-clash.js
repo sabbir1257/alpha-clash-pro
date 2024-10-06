e855314ca1a9f73f58a193c179570de190c45061
sabbir1257 <masabbir1257@gmail.com>
2024-10-06 16:42:08 +0600

    Change Home Screen to Playground by click

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,17 @@
-function play(){
-     console.log('play start now');
-     
+// function play(){
+//      // step-1: hide the home screen. to hide the screen add the class hidden to the home section
+//      const homeSection = document.getElementById('home-screem');
+//      homeSection.classList.add('hidden');
+//      // console.log(homeSection);
+
+//      // show the playground
+//      const playgroundSection = document.getElementById('play-ground');
+//      playgroundSection.classList.remove('hidden');
+//      // console.log(playgroundSection.classList);
+// }
+
+
+function play (){
+     hiddenElementById('home-screen');
+     showElementById('play-ground');
 }
